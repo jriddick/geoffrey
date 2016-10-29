9bce87ed81ffa77c53ce0cace9b9aaec02349566
jriddick <apersson.93@gmail.com>
2016-10-30 00:02:46 +0200

    added bot read handler function

--- bot.go
+++ bot.go
@@ -3,6 +3,7 @@ package geoffrey
 import (
     "github.com/jriddick/geoffrey/irc"
     "github.com/jriddick/geoffrey/msg"
+	"fmt"
 )
 
 type Bot struct {
@@ -32,4 +33,14 @@ func NewBot(config Config) *Bot {
     bot.reader = bot.client.Reader()
 
     return bot
-}  
+} 
+
+func (b *Bot) Handler() {
+    for msg := range b.reader {
+        // Send nick and user after connecting
+        if msg.Trailing == "*** Looking up your hostname..." {
+            b.writer <- fmt.Sprintf("NICK %s", b.config.Nick)
+            b.writer <- fmt.Sprintf("USER %s 0 * :%s", b.config.User, b.config.Name)
+        }
+    }
+}
