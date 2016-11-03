053867fe677a35bbb24012edf7b94c87b35af11f
jriddick <apersson.93@gmail.com>
2016-11-03 18:34:40 +0100

    added bot user

---
+++ geoffrey/user.go
@@ -0,0 +1,12 @@
+package geoffrey
+
+// User represents an IRC user
+type User struct {
+	Nick string
+	bot  *Bot
+}
+
+// Send will send a message to the user
+func (u *User) Send(msg string) {
+	u.bot.Send(u.Nick, msg)
+}
