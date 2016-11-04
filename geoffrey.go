ccdbbe32f1469d67f23d931a9fcf68e3cb1e8dca
jriddick <apersson.93@gmail.com>
2016-11-04 17:50:27 +0100

    added geoffrey.lua and logrus

--- geoffrey.go
+++ geoffrey.go
@@ -1,11 +1,24 @@
 package main
 
 import (
-	"log"
+	"os"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/jriddick/geoffrey/modules/geoffrey"
 	"github.com/yuin/gopher-lua"
 )
+
+var (
+	log = logrus.New()
+)
+
+func init() {
+	// Output to stderr
+	log.Out = os.Stderr
+
+	// Set minimum log level
+	log.Level = logrus.DebugLevel
+}
 
 func main() {
 	state := lua.NewState(lua.Options{
@@ -16,10 +29,15 @@ func main() {
 	defer state.Close()
 
 	// Add the geoffrey module
-	state.PreloadModule("geoffrey", geoffrey.Load)
+	geoffrey.Register(state)
 
 	// Load config.lua
 	if err := state.DoFile("config.lua"); err != nil {
+		log.Fatalln(err)
+	}
+
+	// Load geoffrey.lua
+	if err := state.DoFile("geoffrey.lua"); err != nil {
 		log.Fatalln(err)
 	}
 }
---
+++ geoffrey.lua
@@ -0,0 +1,10 @@
+-- Add a bot
+geoffrey.add("geoffrey", {
+    Hostname = "localhost",
+    Port = 6667,
+    Secure = false,
+    InsecureSkipVerify = false,
+    Nick = "geoffrey",
+    User = "geoffrey",
+    Name = "geoffrey"
+})
