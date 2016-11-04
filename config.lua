4624d31f7cf539c1637a1f765a53541355ade7b4
jriddick <apersson.93@gmail.com>
2016-11-04 17:49:19 +0100

    cleaned config.lua

--- config.lua
+++ config.lua
@@ -1,11 +1,5 @@
-local geoffrey = require("geoffrey")
-
-geoffrey.add("geoffrey", {
-    Hostname = "localhost",
-    Port = 6667,
-    Secure = false,
-    InsecureSkipVerify = false,
-    Nick = "geoffrey",
-    User = "geoffrey",
-    Name = "geoffrey"
-})
+log = {
+    console = true,
+    file = "",
+    level = DEBUG,
+}
