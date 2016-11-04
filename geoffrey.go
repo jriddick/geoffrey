6a40cdf440084b8c468917edf56f96848f31cf54
jriddick <apersson.93@gmail.com>
2016-11-05 00:35:14 +0100

    added support for graceful shutdown

--- geoffrey.go
+++ geoffrey.go
@@ -3,15 +3,40 @@ package main
 import (
 	"os"
 
+	"os/signal"
+	"syscall"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/jriddick/geoffrey/modules/geoffrey"
 	"github.com/yuin/gluamapper"
 	"github.com/yuin/gopher-lua"
 )
 
+var (
+	sigs = make(chan os.Signal, 1)
+	bots *geoffrey.Geoffrey
+)
+
 func init() {
 	// Output to stderr
 	log.SetOutput(os.Stderr)
+
+	// Set the log level to debug
+	log.SetLevel(log.DebugLevel)
+
+	// Capture signals
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+
+	go func() {
+		// Wait until we get a signal
+		<-sigs
+
+		// Shutdown the bot
+		bots.Shutdown()
+
+		// Exit the program
+		os.Exit(1)
+	}()
 }
 
 type config struct {
@@ -26,7 +51,8 @@ func main() {
 	defer state.Close()
 
 	// Add the geoffrey module
-	geoffrey.Register(state)
+	bots := geoffrey.NewGeoffrey()
+	bots.Register(state)
 
 	// Load const.lua
 	if err := state.DoFile("const.lua"); err != nil {
@@ -47,5 +73,11 @@ func main() {
 	// Load geoffrey.lua
 	if err := state.DoFile("geoffrey.lua"); err != nil {
 		log.Fatalln(err)
+	}
+
+	log.Println("geoffrey is now running")
+
+	// Block until program exists (Ctrl-C)
+	for {
 	}
 }
