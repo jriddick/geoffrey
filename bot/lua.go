ad57387024ba01ea92f50d945167465a6eae22bc
jriddick <apersson.93@gmail.com>
2016-11-05 18:46:07 +0100

    added lua bindings for bot

---
+++ bot/lua.go
@@ -0,0 +1,37 @@
+package bot
+
+import (
+	"github.com/yuin/gopher-lua"
+)
+
+// RegisterBot will register the Bot struct to Lua
+func RegisterBot(state *lua.LState) {
+	// Create the Metatable
+	meta := state.NewTypeMetatable("bot")
+	state.SetGlobal("bot", meta)
+
+	// Bind our functions
+	state.SetField(meta, "__index", state.SetFuncs(state.NewTable(), map[string]lua.LGFunction{
+		"send": botSend,
+	}))
+}
+
+// checkBot will check wether the first argument is a *Bot
+// or not.
+func checkBot(state *lua.LState) *Bot {
+	data := state.CheckUserData(1)
+	if v, ok := data.Value.(*Bot); ok {
+		return v
+	}
+	state.ArgError(1, "bot expected")
+	return nil
+}
+
+func botSend(state *lua.LState) int {
+	bot := checkBot(state)
+	rcv := state.CheckString(2)
+	msg := state.CheckString(3)
+
+	bot.Send(rcv, msg)
+	return 0
+}
