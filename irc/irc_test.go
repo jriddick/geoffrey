b7fd3b49efdfd91854d05f12f87d086bb5d6a6cc
jriddick <apersson.93@gmail.com>
2016-11-09 22:30:21 +0100

    added a few more irc client tests

--- irc/irc_test.go
+++ irc/irc_test.go
@@ -20,23 +20,22 @@ var (
 )
 
 func TestClient(t *testing.T) {
+	// Create the mockd server
+	mockd := mockd.NewMockd(5000)
+
+	// Start listening
+	mockd.Listen()
+
+	// Start accepting connections
+	go mockd.Handle()
+
 	Convey("With the default IRC client", t, func() {
-		// Create the mockd server
-		mockd := mockd.NewMockd(5000)
-
-		// Start listening
-		So(mockd.Listen(), ShouldBeNil)
-
-		// Start accepting connections
-		go mockd.Handle()
-
 		Convey("It should be able to connect", func() {
 			// Open client
 			client := NewIRC(defaultConfig)
 
 			// Connect the client
 			So(client.Connect(), ShouldBeNil)
-			defer client.Disconnect()
 
 			// Get the reader channel
 			reader := client.Reader()
@@ -51,7 +50,6 @@ func TestClient(t *testing.T) {
 
 			// Connect to the server
 			So(client.Connect(), ShouldBeNil)
-			defer client.Disconnect()
 
 			// Get the reader and writer channel
 			reader := client.Reader()
@@ -78,7 +76,6 @@ func TestClient(t *testing.T) {
 
 			// Connect
 			So(client.Connect(), ShouldBeNil)
-			defer client.Disconnect()
 
 			// Get the reader
 			reader := client.Reader()
@@ -93,8 +90,87 @@ func TestClient(t *testing.T) {
 			So(<-reader, ShouldNotBeNil)
 		})
 
-		Reset(func() {
-			So(mockd.Stop(), ShouldBeNil)
+		Convey("It should reject empty hostname", func() {
+			// Open client
+			client := NewIRC(Config{
+				Hostname:           "",
+				Port:               defaultConfig.Port,
+				Secure:             defaultConfig.Secure,
+				InsecureSkipVerify: defaultConfig.InsecureSkipVerify,
+				Timeout:            defaultConfig.Timeout,
+				TimeoutLimit:       defaultConfig.TimeoutLimit,
+			})
+
+			// Should fail to connect
+			So(client.Connect(), ShouldNotBeNil)
+		})
+
+		Convey("It should fail to connect securely to non-ssl server", func() {
+			// Create the client
+			client := NewIRC(Config{
+				Hostname:           defaultConfig.Hostname,
+				Port:               defaultConfig.Port,
+				Secure:             true,
+				InsecureSkipVerify: defaultConfig.InsecureSkipVerify,
+				Timeout:            defaultConfig.Timeout,
+				TimeoutLimit:       defaultConfig.TimeoutLimit,
+			})
+
+			// Should fail to connect
+			So(client.Connect(), ShouldNotBeNil)
+		})
+
+		Convey("It should not be able to connect twice", func() {
+			// Create the client
+			client := NewIRC(defaultConfig)
+
+			// Should succeed to connect
+			So(client.Connect(), ShouldBeNil)
+
+			// Should not succeed a second time
+			So(client.Connect(), ShouldNotBeNil)
+		})
+
+		Convey("It should not be able to send empty messages", func() {
+			// Create the client
+			client := NewIRC(defaultConfig)
+
+			// Should succeed to connect
+			So(client.Connect(), ShouldBeNil)
+
+			// Get the writer
+			writer := client.Writer()
+
+			// Send empty message
+			writer <- ""
+
+			// Get error channel
+			errors := client.Errors()
+
+			// We should receive error
+			err := <-errors
+
+			// Check the error
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "tried to send empty message")
+		})
+
+		Convey("It should be able to disconnect", func() {
+			// Create the client
+			client := NewIRC(Config{
+				Hostname:           defaultConfig.Hostname,
+				Port:               defaultConfig.Port,
+				Secure:             defaultConfig.Secure,
+				InsecureSkipVerify: defaultConfig.InsecureSkipVerify,
+				Timeout:            time.Second * 1,
+				TimeoutLimit:       defaultConfig.TimeoutLimit,
+			})
+
+			// Should connect
+			So(client.Connect(), ShouldBeNil)
+
+			// Should disconnect
+			client.Disconnect()
 		})
 	})
 }
