84efce873e6d1ba154e4bfdc2e43872e32556d60
jriddick <apersson.93@gmail.com>
2017-06-19 18:07:42 +0200

    Add IRC color helper

---
+++ irc/colors.go
@@ -0,0 +1,34 @@
+package irc
+
+import "fmt"
+
+// Color codes for IRC messages
+const (
+	White = iota
+	Black
+	Blue
+	Green
+	Red
+	Brown
+	Purple
+	Orange
+	Yellow
+	LightGreen
+	Teal
+	LightCyan
+	LightBlue
+	Pink
+	Grey
+	LightGrey
+	Default
+)
+
+// Foreground sets foreground color of the text to the given color
+func Foreground(text string, code int) string {
+	return fmt.Sprintf("\x03%d%s\x03", code, text)
+}
+
+// Bold makes the text bold
+func Bold(text string) string {
+	return fmt.Sprintf("\x02%s\x02", text)
+}
